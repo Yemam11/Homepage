f93a91830418457c9cee300c9a4a579a5d602237
Yemam11 <1youssefemam@gmail.com>
2023-10-19 13:26:23 -0400

    Added new cube glb

--- converter3D.py
+++ converter3D.py
@@ -4,7 +4,7 @@ import aspose.threed as a3d
 def stl_to_glb(path):
     input = path
     scene = a3d.Scene.from_file(input)
-    scene.save("output.glb")
+    scene.save("3D Models\cube.glb")
 
 
 def stl_to_usdz(path):
@@ -23,6 +23,6 @@ def glb_to_stl(path):
     scene.save("3D Models\testing.stl")
 
 
-glb_to_usdz("3D Models\output.glb")
+glb_to_usdz("3D Models\cube.glb")
 
 #TODO: find some way to make the names change with successive file uploads/standardize the naming of the output files
