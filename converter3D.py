30153506660ee74fb733252b387f9953a9cbc75d
Yemam11 <1youssefemam@gmail.com>
2023-10-19 14:25:40 -0400

    Added textured cube to ios src

--- converter3D.py
+++ converter3D.py
@@ -4,7 +4,7 @@ import aspose.threed as a3d
 def stl_to_glb(path):
     input = path
     scene = a3d.Scene.from_file(input)
-    scene.save("3D Models\texturedcube.glb")
+    scene.save("3D Models\cubetexture.glb")
 
 
 def stl_to_usdz(path):
@@ -15,7 +15,7 @@ def stl_to_usdz(path):
 def glb_to_usdz(path):
     input = path
     scene = a3d.Scene.from_file(input)
-    scene.save("3D Models\cube.usdz")
+    scene.save("3D Models\cubetexture.usdz")
 
 def glb_to_stl(path):
     input = path
@@ -23,6 +23,6 @@ def glb_to_stl(path):
     scene.save("3D Models\testing.stl")
 
 
-stl_to_glb("3D Models\texturedcube.stl")
+glb_to_usdz("3D Models\cubetexture.glb")
 
 #TODO: find some way to make the names change with successive file uploads/standardize the naming of the output files
