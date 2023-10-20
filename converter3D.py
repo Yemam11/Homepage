e9bf9006f40a76dcc17acc8cb7e0b21907966ca2
Yemam11 <1youssefemam@gmail.com>
2023-10-20 13:00:59 -0400

    Added hinge GLB

--- converter3D.py
+++ converter3D.py
@@ -1,27 +1,17 @@
 import aspose.threed as a3d
 
 
-def stl_to_glb(path):
+def threeds_to_glb(path):
     input = path
     scene = a3d.Scene.from_file(input)
-    scene.save("3D Models\cubetexture.glb")
+    scene.save("3D Models\hest.glb")
 
-
-def stl_to_usdz(path):
-    input = path
-    scene = a3d.Scene.from_file(input)
-    scene.save("output.usdz")
 
 def glb_to_usdz(path):
     input = path
     scene = a3d.Scene.from_file(input)
     scene.save("3D Models\cubetexture.usdz")
 
-def glb_to_stl(path):
-    input = path
-    scene = a3d.Scene.from_file(input)
-    scene.save("3D Models\testing.stl")
-
-glb_to_usdz("3D Models\cubetexture.glb")
+threeds_to_glb("3D Models\hest.3ds")
 
 #TODO: find some way to make the names change with successive file uploads/standardize the naming of the output files
